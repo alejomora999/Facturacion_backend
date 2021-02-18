0c34d9e3b6509dabdd4fef37cf5eb3049666ab58
Alejandro Morales <37442544+alejomora999@users.noreply.github.com>
2021-02-18 08:11:35 -0500

    Se ponen cdredenciales generales

--- Backend/correo.sh
+++ Backend/correo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sendemail -f jalejo999@gmail.com -t jalejo999@gmail.com amorales@voxoni.com -s smtp.gmail.com:587 -u \
-"Asunto test" -m "Cuerpo del mensaje" -a adjunto.txt -v -xu jalejo999 -xp delospelos999 -o tls=yes
+sendemail -f origen@gmail.com -t destino1@gmail.com destino2@gmail.com -s smtp.gmail.com:587 -u \
+"Asunto test" -m "Cuerpo del mensaje" -a adjunto.txt -v -xu origen -xp password -o tls=yes
 
